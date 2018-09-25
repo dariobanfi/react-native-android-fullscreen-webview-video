64c673be3f66480e67f876962345bc1db4b8f998
Dario Banfi <dario.banfi9@gmail.com>
2018-09-25 10:36:00 +0200

    Correctly clearing immersive mode flags

--- android/src/main/java/com/airship/customwebview/VideoWebChromeClient.java
+++ android/src/main/java/com/airship/customwebview/VideoWebChromeClient.java
@@ -94,8 +94,7 @@ public class VideoWebChromeClient extends WebChromeClient {
     ((WebView) mWebView).loadUrl("javascript:document.getElementsByTagName('video')[0].pause()");
 
     // Exiting immersive mode
-    mActivity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    mActivity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
 
     mVideoView = null;
     mCustomViewCallback.onCustomViewHidden();
